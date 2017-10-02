542538dff1944a44f2245da6431b8b1a4c219245
TarfaM <tarfa.mansour@gmail.com>
2017-10-02 14:27:49 +0300

    update index js file
    update index js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-//index file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBox from './components/search_bar';
+
+
+//create a componet
+const App = () => {
+
+  return (
+<div>
+    <SearchBox />
+    </div>
+  );
+}
+
+//take this componet's generated HTML and put it in DOM
+
+ReactDOM.render(<App /> , document.querySelector('.container'));
