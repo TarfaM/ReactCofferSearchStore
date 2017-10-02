2643cab062615f33eda512e4a73f230e6a121c96
TarfaM <tarfa.mansour@gmail.com>
2017-10-02 14:26:41 +0300

    Submit search bar code
    Submit search bar code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,23 @@
 // search bar
+import React, {Component} from 'react';
+
+
+//using State
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {term: '' };
+  };
+render(){
+  return (
+  <div>
+  <input onChange ={event => this.setState ({term: event.target.value})}/>
+Value of the input : {this.state.term}
+  </div>
+);
+}
+}
+//End using State
+
+  //exporting the elemanet
+export default SearchBar;
